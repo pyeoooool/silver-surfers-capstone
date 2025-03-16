9e207413ff1015048a49b12f0ea71f7e5d41d956
pyeoooool <piolodelossantos3@gmail.com>
2025-03-16 14:54:57 +0800

    some more CSS adjustments for respoinsiveness

--- scripts/posts.js
+++ scripts/posts.js
@@ -18,7 +18,7 @@ export const post = [{
   whoPosted: 'How we maintain our relationship for 30 years',
   age: 67,
   location: 'Bernadette Umali',
-  pfp: '/profile-pictures/random-woman-1.jpg', //file path
+  pfp: '/profile-pictures/random-woman-2.jpg', //file path
   postUserName: 'dionela_1',
   image: '../uploads-images/stock-image-2.jpg', //file path
   likeCount: 2,
@@ -31,7 +31,7 @@ export const post = [{
   whoPosted: 'Growing business in the digital age as a senior citizen',
   age: 62,
   location: 'Nicole Lopez',
-  pfp: '/profile-pictures/random-woman-1.jpg', //file path
+  pfp: '/profile-pictures/random-woman-3.jpg', //file path
   postUserName: 'dionela_1',
   image: '../uploads-images/stock-image-2.jpg', //file path
   likeCount: 2,
@@ -39,6 +39,45 @@ export const post = [{
   caption: `Sharing my tips on how to be healthy in your 60s!`,
   timePosted: '',
   datePosted: ''
+}, {
+  id: 1,
+  whoPosted: 'My top 10 tips for a healthy lifestyle',
+  age: 65,
+  location: 'Angelica Parilla',
+  pfp: '/profile-pictures/random-woman-1.jpg', //file path
+  postUserName: 'dionela_1',
+  image: '../uploads-images/stock-image-1.jpg', //file path
+  likeCount: 2,
+  commentCount: 2,
+  caption: `Prepare some coffee, and let's dive into my world!`,
+  timePosted: '12:43 PM',
+  datePosted: 'March 13, 2025'
+}, {
+  id: 1,
+  whoPosted: 'My top 10 tips for a healthy lifestyle',
+  age: 65,
+  location: 'Angelica Parilla',
+  pfp: '/profile-pictures/random-woman-1.jpg', //file path
+  postUserName: 'dionela_1',
+  image: '../uploads-images/stock-image-1.jpg', //file path
+  likeCount: 2,
+  commentCount: 2,
+  caption: `Prepare some coffee, and let's dive into my world!`,
+  timePosted: '12:43 PM',
+  datePosted: 'March 13, 2025'
+}, {
+  id: 1,
+  whoPosted: 'My top 10 tips for a healthy lifestyle',
+  age: 65,
+  location: 'Angelica Parilla',
+  pfp: '/profile-pictures/random-woman-1.jpg', //file path
+  postUserName: 'dionela_1',
+  image: '../uploads-images/stock-image-1.jpg', //file path
+  likeCount: 2,
+  commentCount: 2,
+  caption: `Prepare some coffee, and let's dive into my world!`,
+  timePosted: '12:43 PM',
+  datePosted: 'March 13, 2025'
 }];
 
 /*
